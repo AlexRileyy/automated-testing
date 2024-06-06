330cc738089afa62781ada9e9d7fc5c416733ac0
2405-003-AR <2405-003-ar@U-12O1ZPNDP3S9B.corp.amazonworkspaces.com>
2024-06-06 16:05:16 +0100

    first commit

--- src/find-longest-string.test.js
+++ src/find-longest-string.test.js
@@ -8,4 +8,6 @@ test("findLongestString finds the longest string in an array", () => {
 
 test("findLongestString returns the earlier string in cases of joint longest strings", () => {
   expect(findLongestString(["brave", "dance"])).toBe("brave");
+  expect(findLongestString(["peak", "chop"])).toBe("peak");
+  expect(findLongestString(["man", "van"])).toBe("man");
 });
